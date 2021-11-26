a31cc1b86ff49716a238ab9d37a00bd4fd71e969
sagakanon2021 <94866651+sagakanon2021@users.noreply.github.com>
2021-11-26 18:54:00 -0500

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,16 @@
+//declarar funciones
+//valor numero1=5;
+
+//------------------------------
+
+//DECLARAR funciones
+
+function sumar(valor1,valor2){
+ var resultado= valor1+valor2;
+ if (resultado >=12){
+   alert(resultado);
+ } else{
+   document.write(resultado);
+ }
+}
+sumar(5,3);
